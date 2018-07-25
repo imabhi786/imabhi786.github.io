02181527db0fb7cc425e4a7ab4cf8c190e9342ac
Abhijeet Mathur <abhijeetmathur786@gmail.com>
2018-07-25 21:51:55 +0530

    mobile view update

--- js/main.js
+++ js/main.js
@@ -8,9 +8,3 @@ function myFunction() {
         document.querySelector(".navbar").classList.remove("scrolled");
     }
 }
-var gradient = document.querySelector(".gradient");
-
-function onMouseMove(event) {
-  gradient.style.backgroundImage = 'radial-gradient(at ' + event.clientX + 'px ' + event.clientY + 'px, rgba(159,0,191,.9) 0, #4D4FA7 70%)';
-}
-document.addEventListener("mousemove", onMouseMove);
