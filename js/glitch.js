3d26bf2f2ab3a673553b64d65af547ac9861a9c9
Abhijeet Mathur <abhijeetmathur786@gmail.com>
2018-12-20 17:39:37 +0530

    Fix all issues and add contact

---
+++ js/glitch.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+  //your code here
+  $("header").append("<div class='glitch-window'></div>");
+//fill div with clone of real header
+$( "h1.glitched" ).clone().appendTo( ".glitch-window" );
+});
+
